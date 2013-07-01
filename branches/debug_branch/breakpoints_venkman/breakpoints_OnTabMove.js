bae5ecfe1189fbfe361ec101019c10b899b4f9ca
miguelromero2000@gmail.com <miguelromero2000@gmail.com@c81f0d1b-9a01-45ae-5343-9ffd5e3aaede>
2013-07-01 21:23:06 +0000

    - Added breakpoints to try to catch the tab disappearing bug when moving a tab. breakpoints_OnTabMove.js

---
+++ branches/debug_branch/breakpoints_venkman/breakpoints_OnTabMove.js
@@ -0,0 +1,102 @@
+
+//Breakpoint settings start...
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:282, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:300, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:306, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1148, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1151, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1197, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1539, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1581, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1604, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1610, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:1616, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2111, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2117, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2162, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2166, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2178, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2263, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2332, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2335, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2459, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2475, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2512, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2515, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2759, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2927, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2898, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2976, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:2988, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:3110, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:3132, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:3349, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:5030, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+dispatch('fbreak', ({urlPattern:"chrome://tabgroupsmanager/content/TabGroupsManager.js", lineNumber:5318, properties:{conditionEnabled:false, condition:"", passExceptions:false, logResult:false, resultAction:1}}));
+
+
+"Breakpoints restored.";
